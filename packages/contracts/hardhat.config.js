813d55630c2ac1797a2dfb895b5082e65527e994
Gabriel Buragev <gabriel.buragev96@gmail.com>
2023-09-03 00:59:38 +0200

    configure contract deployment

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -1,7 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ethers");
-
+require("dotenv/config");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mantle: {
+      url: "https://mantle.publicnode.com",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
